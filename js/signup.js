7a78f9e1ed69077924579a6b47a512bc8f4f6767
Jon <jonegeland@gmail.com>
2018-01-12 00:07:46 -0500

    Working autocompletes for game submissions.

--- js/signup.js
+++ js/signup.js
@@ -1,23 +1,32 @@
 horsey(document.querySelector('.signup__find-game__input'), {
   source: [{ list: [
-    { value: 'sm64',    text: 'Super Mario 64 - 120 Star' },
-    { value: 'sms',     text: 'Super Mario Sunshine - 120 Shine' },
-    { value: 'smg1',    text: 'Super Mario Galaxy 1 - 120 Star' },
-    { value: 'smg2',    text: 'Super Mario Galaxy 2 - 242 Star' },
-    { value: 'kazooie', text: 'Banjo Kazooie - 100%' },
-    { value: 'tooie',   text: 'Banjo Tooie - 100%' },
-    { value: 'dk64',    text: 'Donkey Kong 64 - 101%' },
-    { value: 'crash1',  text: 'Crash 1 - 100%' },
-    { value: 'crash2',  text: 'Crash 2 - 100%' },
-    { value: 'crash3',  text: 'Crash 3 - 105%' },
-    { value: 'spyro1',  text: 'Spyro 1 - 120%' },
-    { value: 'spyro2',  text: 'Spyro 2 - 100%' },
-    { value: 'spyro3',  text: 'Spyro 3 - 117%' }
+    { id: 'sm64',    game: 'Super Mario 64', category: '120 Star' },
+    { id: 'sms',     game: 'Super Mario Sunshine', category: '120 Shine' },
+    { id: 'smg1',    game: 'Super Mario Galaxy 1', category: '120 Star' },
+    { id: 'smg2',    game: 'Super Mario Galaxy 2', category: '242 Star' },
+    { id: 'kazooie', game: 'Banjo Kazooie', category: '100%' },
+    { id: 'tooie',   game: 'Banjo Tooie', category: '100%' },
+    { id: 'dk64',    game: 'Donkey Kong 64', category: '101%' },
+    { id: 'crash1',  game: 'Crash 1', category: '100%' },
+    { id: 'crash2',  game: 'Crash 2', category: '100%' },
+    { id: 'crash3',  game: 'Crash 3', category: '105%' },
+    { id: 'spyro1',  game: 'Spyro 1', category: '120%' },
+    { id: 'spyro2',  game: 'Spyro 2', category: '100%' },
+    { id: 'spyro3',  game: 'Spyro 3', category: '117%' }
   ]}],
-  getText: 'text',
-  getValue: 'value',
+  getText: 'game',
+  getValue: 'id',
   renderItem: function (li, suggestion) {
-    var image = '<img class="autocomplete-boxart" src="res/' + suggestion.value + '.jpg" /> ';
-    li.innerHTML = image + suggestion.text;
-  }
+    li.innerHTML =
+      '<img class="autocomplete__boxart" src="res/' + suggestion.id + '.jpg" />' +
+      '<span class="autocomplete__content">' +
+        '<span class="autocomplete__game">' + suggestion.game + '</span>' +
+        ' - ' +
+        '<span class="autocomplete__category">' + suggestion.category + '</span>' +
+      '</span>';
+  },
+  noMatches: "No matches",
+  debounce: 50,
+  appendTo: document.querySelector(".signup__find-game"),
+  limit: 5
 });
