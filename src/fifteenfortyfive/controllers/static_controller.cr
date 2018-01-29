8e078a7105f8f5210dbce83b372a30faf812d2d4
Jon <jonegeland@gmail.com>
2018-01-28 20:13:25 -0500

    Add submission counts to the index.

--- src/fifteenfortyfive/controllers/static_controller.cr
+++ src/fifteenfortyfive/controllers/static_controller.cr
@@ -2,6 +2,8 @@ module StaticController
   extend self
 
   def index(env)
+    runner_submissions = Repo.all(RunnerSubmission)
+    commentator_submissions = Repo.all(CommentatorSubmission)
     render_view "static/index"
   end
 end
