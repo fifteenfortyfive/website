c85d81bd005ad345951f738b03dab465cd6c30ee
Jon <jonegeland@gmail.com>
2018-02-09 11:06:21 -0500

    Use discord.fifteenfortyfive.org as discord link

--- src/fifteenfortyfive/constants.cr
+++ src/fifteenfortyfive/constants.cr
@@ -1,5 +1,5 @@
 module Constants
-  DISCORD_URL = "https://discord.gg/ZYfBpdk"
+  DISCORD_URL = "http://discord.fifteenfortyfive.org"
   TWITCH_URL  = "https://twitch.tv/1545race"
   TWITTER_URL = "https://twitter.com/1545race"
 end
