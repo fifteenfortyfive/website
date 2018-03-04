8fc5e55244ae617fd62fa8a585e3901c32e18d08
Jon <jonegeland@gmail.com>
2018-03-03 23:50:35 -0500

    Basic submissions table layout.

--- src/fifteenfortyfive/controllers/admin/submissions_controller.cr
+++ src/fifteenfortyfive/controllers/admin/submissions_controller.cr
@@ -3,13 +3,14 @@ module Admin::SubmissionsController
   extend self
 
   def index(env)
-    unless env.current_user? && env.current_user.admin
-      render_404
-      return
-    end
-
     runner_submissions      = Repo.all(RunnerSubmission, Query.order_by("updated_at DESC"), preload: [:account])
     commentator_submissions = Repo.all(CommentatorSubmission, Query.order_by("updated_at DESC"), preload: [:account])
     render_view "admin/submissions/index"
+  end
+
+  def runners(env)
+    runner_submissions      = Repo.all(RunnerSubmission, Query.order_by("updated_at DESC").where(revoked: "false"), preload: [:account])
+
+    render_view "admin/submissions/runners"
   end
 end
--- src/fifteenfortyfive/router.cr
+++ src/fifteenfortyfive/router.cr
@@ -43,7 +43,8 @@ scope "/admin" do
 
   get "/accounts", &->Admin::AccountsController.index(Krout::Env)
 
-  get "/submissions", &->Admin::SubmissionsController.index(Krout::Env)
+  get "/submissions",         &->Admin::SubmissionsController.index(Krout::Env)
+  get "/submissions/runners", &->Admin::SubmissionsController.runners(Krout::Env)
 end
 
 
