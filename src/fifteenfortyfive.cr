9bd275916e39898c0f9c00be7812fe2cbf1aa589
Jon <jonegeland@gmail.com>
2018-05-06 14:53:04 -0400

    SUPERVISE!

--- src/fifteenfortyfive.cr
+++ src/fifteenfortyfive.cr
@@ -3,15 +3,21 @@ require "krout"
 require "kilt/slang"
 require "pg"
 require "crecto"
+require "honcho"
 
 require "./fifteenfortyfive/repo.cr"
 require "./fifteenfortyfive/controllers/base_controller.cr"
 require "./fifteenfortyfive/**"
 
+require "./supervisor.cr"
+
 add_handler SessionHandler.new
 add_handler FeatureFlagHandler.new
 
 
-StreamStatusService.run_in_background
+visor = Honcho::Visor.new(strategy: Honcho::Strategy::ISOLATED)
 
-Kemal.run
+visor.start_supervised("stream statuses", &->StreamStatusService.run)
+visor.start_supervised("kemal", &->Kemal.run)
+
+visor.run
