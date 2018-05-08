a2d6d7279ffabe673b86250bcf3ee9c0a32cd7ad
Jon <jonegeland@gmail.com>
2018-05-08 11:52:00 -0400

    Add restart delay to stream status service

--- src/fifteenfortyfive.cr
+++ src/fifteenfortyfive.cr
@@ -17,7 +17,9 @@ add_handler FeatureFlagHandler.new
 
 visor = Honcho::Visor.new(strategy: Honcho::Strategy::ISOLATED)
 
-visor.start_supervised("stream statuses", &->StreamStatusService.run)
+# Delay restarting the stream status service by one full rate-limit window to avoid
+# an infinite loop of timeouts.
+visor.start_supervised("stream statuses", delay: 60.0, &->StreamStatusService.run)
 visor.start_supervised("kemal", &->Kemal.run)
 
 visor.run
