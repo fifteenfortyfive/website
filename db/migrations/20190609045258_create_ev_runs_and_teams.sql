eaec9cef6bf7189fda0024492f3ed3f32c559e92
Jon <jonegeland@gmail.com>
2019-06-09 13:32:50 -0700

    captains

--- db/migrations/20190609045258_create_ev_runs_and_teams.sql
+++ db/migrations/20190609045258_create_ev_runs_and_teams.sql
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS "public"."ev_runs" (
   index integer,
   run_submission_id integer,
   account_id integer,
-  events_id integer,
+  event_id integer,
   team_id integer,
   game_id integer,
   created_at timestamp DEFAULT now(),
   updated_at timestamp DEFAULT now(),
   FOREIGN KEY ("run_submission_id") REFERENCES "public"."run_submissions"("id"),
   FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("id"),
-  FOREIGN KEY ("events_id") REFERENCES "public"."events"("id"),
+  FOREIGN KEY ("event_id") REFERENCES "public"."events"("id"),
   FOREIGN KEY ("game_id") REFERENCES "public"."games"("id"),
   FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id")
 );
