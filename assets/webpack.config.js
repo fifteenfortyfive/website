cc32cfe15a2468adedfcb81e5ffcf85dd3a64161
Jon <jonegeland@gmail.com>
2019-11-26 00:31:58 -0800

    better css modules identifiers

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
           'style-loader',
           {
             loader: 'css-loader',
-            options: {modules: true}
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
+            },
           },
           'postcss-loader'
         ]
