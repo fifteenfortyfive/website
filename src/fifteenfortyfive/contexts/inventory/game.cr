e42cd2ff70ce793a0c2e1bedb0dc885362686fab
Jon <jonegeland@gmail.com>
2020-01-10 18:55:08 -0500

    add giantbomb game scraping with extra fields
    Probably not gnona use this for now, but it's a nice way to populate the games db quickly and accurately with aliases and such.

--- src/fifteenfortyfive/contexts/inventory/game.cr
+++ src/fifteenfortyfive/contexts/inventory/game.cr
@@ -10,6 +10,10 @@ module Inventory
       field :sequence_number, Int32
 
       field :twitch_id, String
+      field :giantbomb_id, Int32
+
+      field :description, String
+      field :aliases, Array(String)
 
       has_many :categories, Category
     end
