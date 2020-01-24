a81611f47bfbdb226b8f004767d2ec4baf611ecb
Jon <jonegeland@gmail.com>
2020-01-23 19:37:15 -0800

    Fix naming of service definition to mcsn_web

--- mcsn_supervisor/mcsn/services/mcsn_web.py
+++ mcsn_supervisor/mcsn/services/mcsn_web.py
@@ -7,19 +7,19 @@ from .. import supervisor
 
 
 def _get_service_dir(root):
-    return os.path.join(root, "mcsn_api")
+    return os.path.join(root, "mcsn_web")
 
 
 def _do_build(ctx):
-    return subprocess.run(["shards", "build", "cli"], cwd=_get_service_dir(ctx.root))
+    return subprocess.run(["yarn", "build", "cli"], cwd=_get_service_dir(ctx.root))
 
 
-@click.group("mcsn_api")
+@click.group("mcsn_web")
 @click.pass_obj
-def mcsn_api(ctx):
+def mcsn_web(ctx):
     """Scripts for the MCSN API service"""
     click.secho("Operating on ", nl=False)
-    click.secho("mcsn_api", fg="cyan", bold=True, nl=False)
+    click.secho("mcsn_web", fg="cyan", bold=True, nl=False)
     click.secho(" service")
 
 
@@ -44,7 +44,7 @@ def build(ctx):
 def start(_ctx, watch):
     """Start the service via supervisord"""
     _ = watch
-    click.echo("Starting mcsn_api via supervisor")
+    click.echo("Starting via supervisor")
     supervisor.start_service("mcsn_basic:mcsn_web")
 
 
@@ -52,7 +52,7 @@ def start(_ctx, watch):
 @click.pass_obj
 def stop(_ctx):
     """Stop the service via supervisord"""
-    click.echo("Stopping mcsn_api via supervisor")
+    click.echo("Stopping via supervisor")
     supervisor.stop_service("mcsn_basic:mcsn_web")
 
 
@@ -60,12 +60,12 @@ def stop(_ctx):
 @click.pass_obj
 def restart(_ctx):
     """Restart the service via supervisord"""
-    click.echo("Restarting mcsn_api via supervisor")
+    click.echo("Restarting via supervisor")
     supervisor.restart_service("mcsn_basic:mcsn_web")
 
 
-mcsn_api.add_command(deps)
-mcsn_api.add_command(build)
-mcsn_api.add_command(start)
-mcsn_api.add_command(stop)
-mcsn_api.add_command(restart)
+mcsn_web.add_command(deps)
+mcsn_web.add_command(build)
+mcsn_web.add_command(start)
+mcsn_web.add_command(stop)
+mcsn_web.add_command(restart)
